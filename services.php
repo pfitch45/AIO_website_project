ed61c376be422c132dc903c00ea2ba0d96d18687
pfitch45 <pfitch45@gmail.com>
2014-09-10 17:13:26 -0700

    wip

--- services.php
+++ services.php
@@ -26,7 +26,7 @@
 					<h3>Services and Information</h3>
 					<h4>Overview</h4>
 					<p>All In One Furniture is a flexible company that excels in piecing together RTA (Ready-to-Assemble) furniture. We also install items such as TV mounts, whiteboards, chatboards, shelves, pictures, etc.</p><br>  
-					<p>All In One is confident with the ability to get the job done right: Whether it's large  and complex like a closet system or full home enterntainment system, or even your backyard patio picnic table--we can get it done.  We also perform office-oriented assembly and installation services and have several years' experience doing so. <br>
+					<p>All In One is confident with the ability to get the job done right: Whether it's large  and complex like a closet system to your backyard patio picnic table--we can get it done.  We also perform office-oriented assembly and installation services and have several years experience doing so. <br>
 					<p>We are very familiar with RTA furniture sold at major retailers including Ikea, West Elm, Staples, Sauder, Coaster, Overstock.com, CB2, Crate and Barrel, South Shore, The Container Store, Bush, Office Max, Easy Closet, Target, Walmart, Office Depot, just to name a few.</p>
 					<h4>Pricing and Availability</h4>
 					<p>All In One offers flat, competitve rates for jobs. The minimum service fee is $59.99.</p>
@@ -34,8 +34,8 @@
 					<p>All In One is quick to respond to service calls after being set up by the customer. We always carry out same-day service whenever possible.</p>
 					<p>Not only is our company quick to start working for you, we are also very flexible with scheduling.  Feel free to call us!</p>
 					<h4>About</h4>
-					<p>All In One Furniture Assembly was started by Alfonso, our company's original handyman. He has over thirteen years of experience fixing, assembling, and delivering a wide varriety of furniture styles, from modern Scandinavian designs to rustic traditonal Mexican pieces to houses and offices.  After working for companies like Macy's, JC Penney, and Ikea, Alfonso noticed that many of his customers&bsp
-						needed help with their furniture purchases, so he decided to start All In One to meet his customers' needs.  Alfonso also has worked closely and exclusively with many local area tech office teams.</p>
+					<p>All In One Furniture Assembly was started by Alfonso, our company's original installer. He has over ten years of experience fixing, assembling, and delivering a wide varriety of furniture styles, from modern Scandinavian designs to rustic traditonal Mexican pieces to houses and offices.  
+						After working for companies like Macy's, JC Penney, and Ikea, Alfonso noticed that many of his customers needed help with their furniture purchases, so he decided to start All In One to meet his customers' needs.  Alfonso also has worked closely and exclusively with many local area tech office teams.</p>
 					<a href="http://www.yelp.com/biz/all-in-one-furniture-assembly-oakland" target="_blank"><img src="images/yelp.jpg"></a>
 				</div>	
 			</div>
